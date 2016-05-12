fbc3615cfa3b8dd47dfab835659633d7152381dc
AlkBur <alkboor@mail.ru>
2016-05-12 16:41:30 +0300

    Added changes

--- init.sh
+++ init.sh
@@ -6,13 +6,12 @@ else
    echo "no path"
 fi
 
-#sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/main.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
-sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/main.conf
 sudo /etc/init.d/gunicorn restart
 
-gunicorn -b 0.0.0.0:8080 hello --chdir ~/web/etc
+#gunicorn -b 0.0.0.0:8080 hello --chdir ~/web/etc
 
